fb2717cabbff61a8eae563be53d9ee969d00d91a
sreeja-star <72139351+sreeja-star@users.noreply.github.com>
2020-12-01 17:41:52 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+red velvet cake process of making red velvetcakeS
---
+++ recipe.js
@@ -0,0 +1,14 @@
+I use specific ingredients, certain amounts, and unique mixing techniques to produce my best red velvet cake recipe. Begin by whisking the dry ingredients together, then beat together the wet ingredients. We’ll combine the two, add buttermilk and tint the batter red. More on food coloring below.
+
+
+
+
+
+
+
+
+
+
+
+
+
